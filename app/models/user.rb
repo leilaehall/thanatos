cc541f6340842f9102396928493c4e89a691916f
Leila Hall <leilaehall@gmail.com>
2019-05-28 12:10:13 +0200

    routes complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
-  has_many :delegates
-  has_many :messages
-  has_many :funeral_preferences
+  has_many :delegates, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :funeral_preferences, dependent: :destroy
   has_many :templates
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
