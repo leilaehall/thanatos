cf64bebd4804539ea17cc76cfd27b49906c61820
Leila Hall <leilaehall@gmail.com>
2019-05-31 16:01:26 +0200

    delegates updated

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,10 +3,40 @@ class PreferencesController < ApplicationController
   end
 
   def funeral
-    if current_user.funeral_preferences
+    if !current_user.funeral_preferences.empty?
       @preference = current_user.funeral_preferences.first
     else
       @preference = FuneralPreference.new
     end
+  end
+
+  def create
+    @preference = FuneralPreference.new(preference_params)
+    @preference.user = current_user
+
+    if @preference.save
+      flash[:notice] = "Your preference have been saved."
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def preference_params
+    params.require(:funeral_preference).permit(
+      :address,
+      :guest_list,
+      :music,
+      :card,
+      :speaker,
+      :reading,
+      :charity,
+      :dress,
+      :hairstyle,
+      :make_up,
+      :embalming
+    )
   end
 end
