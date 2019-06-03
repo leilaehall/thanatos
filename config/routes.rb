20aa69fff3cff11cfb16c02a58b658920f52885e
Chris Hordijk <hordijk.chris@gmail.com>
2019-06-03 17:39:55 +0200

    updated message

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   resources :users
   get '/dashboard', to: 'dashboard#index'
-  resources :messages, only: [:index, :create]
+  resources :messages, only: [:index, :create, :destroy]
 
   resources :delegates, only: [:index, :create, :edit, :destroy] do
     member do
